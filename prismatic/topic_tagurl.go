2b87eedee149961e472f01c1640ec7df464e8f4d
John Wesonga <johnwesonga@gmail.com>
2015-02-20 16:59:33 +0300

    added some godocs

--- prismatic/topic_tagurl.go
+++ prismatic/topic_tagurl.go
@@ -3,3 +3,7 @@ package prismatic
 type UrlTopic struct {
 	Topic []Topic `json:"topics"`
 }
+
+// Tag URL with interests.
+//
+// Prismatic API: https://github.com/Prismatic/interest-graph#tag-url-with-interests.
